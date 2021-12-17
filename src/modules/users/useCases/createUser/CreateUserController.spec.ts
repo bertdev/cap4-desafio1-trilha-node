e0e47f439d83c5cda89456649d7fdb7bf0c488f3
bert <herbertmatos10@hotmail.com>
2021-12-17 17:31:29 -0300

    feat: implementa teste de integracao para o erro ao tentar criar mais de um  usuario com o mesmo email

--- src/modules/users/useCases/createUser/CreateUserController.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -25,4 +25,14 @@ describe("Create User", () => {
 
     expect(response.status).toBe(201);
   });
+
+  it("Should not be able to create a new user with an email that is already in use", async () => {
+    const response = await request(app).post("/api/v1/users").send({
+      name: "Other Name User Test",
+      email: "useremail@test.com",
+      password: "userpasswordtest"
+    });
+
+    expect(response.status).toBe(400);
+  });
 });
