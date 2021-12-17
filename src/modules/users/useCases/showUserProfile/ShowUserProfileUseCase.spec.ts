864ab1572d577706bcc48454b8d7a9560caaf4cc
bert <herbertmatos10@hotmail.com>
2021-12-17 11:34:21 -0300

    feat: implementa teste unitario para exibicao do perfil de um user

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,29 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let showUserProfileUseCase: ShowUserProfileUseCase;
+
+describe("Show User Profile", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+    showUserProfileUseCase = new ShowUserProfileUseCase(inMemoryUsersRepository);
+  });
+
+  it("Should be able to show user profile", async () => {
+    const user = await createUserUseCase.execute({
+      name: "User Name",
+      email: "useremail@test.com",
+      password: "userpasswordtest"
+    });
+
+    const userProfile = await showUserProfileUseCase.execute(user.id as string);
+
+    expect(userProfile).toHaveProperty("id");
+    expect(userProfile).toEqual(user);
+  });
+
+});
